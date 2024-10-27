1e40ca933fcbfcd354c39f0ddd0fc38e4056c6ef
Carlos Severo <carlos.severo@zipbank.com.br>
2024-10-27 11:56:52 -0300

    Implemento os endpoints de tarefas

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,7 +2,7 @@ from dataclasses import asdict
 
 from sqlalchemy import select
 
-from fast_zero.models import User
+from fast_zero.models import Todo, User
 
 
 def test_create_user(session, mock_db_time):
@@ -22,4 +22,21 @@ def test_create_user(session, mock_db_time):
         'email': 'teste@test',
         'created_at': time,
         'updated_at': time,
+        'todos': [],
     }
+
+
+def test_create_todo(session, user: User):
+    todo = Todo(
+        title='Test Todo',
+        description='Test Desc',
+        state='draft',
+        user_id=user.id,
+    )
+
+    session.add(todo)
+    session.commit()
+
+    user = session.scalar(select(User).where(User.id == user.id))
+
+    assert todo in user.todos
